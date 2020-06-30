284f402e2ead9ad1bd36114536c7698b4b9757f5
Vinod Kumar Kayartaya <vinod@vinod.co>
2020-06-30 10:14:45 +0530

    🇮🇳

---
+++ eclipse-workspace/jpa-demo/src/main/java/co/vinod/entity/Category.java
@@ -0,0 +1,25 @@
+package co.vinod.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Category {
+	@Id
+	@Column(name = "category_id")
+	private Integer categoryId;
+	@Column(name = "category_name")
+	private String categoryName;
+	private String description;
+	private byte[] picture;
+}
